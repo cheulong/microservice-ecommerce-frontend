bddcb62bf73f9c33e259851d9a1ec8872a1871b6
Cheulong Sear <searcheulong@gmail.com>
2025-11-24 21:45:37 +0700

    ci: add gitlab ci file

---
+++ apps/product-service/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+const PORT = process.env.PORT || 5000;
+
+app.get("/health", (req, res) => {
+  res.status(200).send("Product Service is healthy");
+});
+
+app.listen(PORT, () => {
+  console.log(`Product Service is running on port ${PORT}`);
+});
